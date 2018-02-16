cb0283dbf703c97816b1f53e16f431809e4b9a3c
Jesus Guevara <desarrollo@designplus.co>
2018-02-16 10:03:57 -0500

    Caracteristica : Lista de testing a una función

---
+++ main.go
@@ -0,0 +1,17 @@
+package main 
+
+import	(
+	"fmt"
+)
+
+func Calculate(x int)(result int){
+	result = x + 2
+	return result
+}
+
+func main() {
+	fmt.Println("Go Testing code")
+	result := Calculate(2)
+
+	fmt.Println(result)
+}
